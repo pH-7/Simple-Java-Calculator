1cb94369edec51d73681a37bd9ca25c678886423
傅浩南 <fhnstephen@gmail.com>
2014-11-08 16:03:56 +0800

    添加test源代码文件夹，添加示例代码

---
+++ Calculator/test/ph/calculator/MainTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package ph.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author fhnstephen
+ *
+ */
+public class MainTest {
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link ph.calculator.Main#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		String[] args = new String[0];
+		Main.main(args);
+	}
+
+}
