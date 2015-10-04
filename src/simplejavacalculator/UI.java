ce40e4428518589850aa0f8555a85064c836588a
Eamon <EamonElH@users.noreply.github.com>
2015-10-04 19:19:16 +0100

    Added array for numeric buttons
    Added arrays for creating numeric JButtons, adding to panel, adding ActionListener, and listening for button.

--- src/simplejavacalculator/UI.java
+++ src/simplejavacalculator/UI.java
@@ -23,44 +23,38 @@ import java.awt.event.ActionEvent;
 
 public class UI implements ActionListener {
 
-    private JFrame frame;
-    private JPanel panel;
-    private JTextArea text;
-    private JButton but1, but2, but3, but4, but5, but6, but7, but8, but9, but0, butAdd, butMinus, butMultiply, butDivide, butEqual, butCancel, butSquareRoot, butSquare, butOneDevidedBy, butCos, butSin, butTan;
-    private Double num1, num2, result;
-    private int add = 0, minus = 0, multiply = 0, divide = 0;
-    
+	private JFrame frame;
+	private JPanel panel;
+	private JTextArea text;
+	private JButton but[], butAdd, butMinus, butMultiply, butDivide, butEqual, butCancel, butSquareRoot, butSquare, butOneDevidedBy, butCos, butSin, butTan;
+	private Double num1, num2, result;
+	private int add = 0, minus = 0, multiply = 0, divide = 0;
+	String[] buttonValue = {"0","1","2","3","4","5","6","7","8","9"};
+
 	public UI() {
 		frame = new JFrame("Calculator PH");
-                frame.setResizable(false);
+		frame.setResizable(false);
 		panel = new JPanel(new FlowLayout());
 		
 		text = new JTextArea(2,25);
-		but1 = new JButton("1");
-                but2 = new JButton("2");
-		but3 = new JButton("3");
-		but4 = new JButton("4");
-		but5 = new JButton("5");
-		but6 = new JButton("6");
-                but7 = new JButton("7");
-		but8 = new JButton("8");
-		but9 = new JButton("9");
-		but0 = new JButton("0");
+		but = new JButton[10];
+		for(int i = 0; i < 10; i++) {
+			but[i] = new JButton(String.valueOf(i));
+		}
 		
 		butAdd = new JButton("+");
 		butMinus = new JButton("-");
 		butMultiply = new JButton("*");
 		butDivide = new JButton("/");
 		butEqual = new JButton("=");
-                butSquareRoot = new JButton("√");
-                butSquare = new JButton("x*x");
-                butOneDevidedBy = new JButton("1/x");
-                butCos = new JButton("Cos");
-                butSin = new JButton("Sin");
-                butTan = new JButton("Tan");
+		butSquareRoot = new JButton("√");
+		butSquare = new JButton("x*x");
+		butOneDevidedBy = new JButton("1/x");
+		butCos = new JButton("Cos");
+		butSin = new JButton("Sin");
+		butTan = new JButton("Tan");
 		
 		butCancel = new JButton("C");
-		
 	}
 	
 	public void init() {
@@ -70,100 +64,48 @@ public class UI implements ActionListener {
 		frame.add(panel);
 		
 		panel.add(text);
-		panel.add(but1);
-		panel.add(but2);
-		panel.add(but3);
-		panel.add(but4);
-		panel.add(but5);
-		panel.add(but6);
-		panel.add(but7);
-		panel.add(but8);
-		panel.add(but9);
-		panel.add(but0);
-		
+		for(int i = 0; i < 10; i++) {
+			panel.add(but[i]);
+			but[i].addActionListener(this);
+		}
+
 		panel.add(butAdd);
 		panel.add(butMinus);
 		panel.add(butMultiply);
 		panel.add(butDivide);
-                panel.add(butSquare);
-                panel.add(butSquareRoot);
-                panel.add(butOneDevidedBy);
-                panel.add(butCos);
-                panel.add(butSin);
-                panel.add(butTan);
-		
-                panel.add(butEqual);
+		panel.add(butSquare);
+		panel.add(butSquareRoot);
+		panel.add(butOneDevidedBy);
+		panel.add(butCos);
+		panel.add(butSin);
+		panel.add(butTan);
+
+		panel.add(butEqual);
 		panel.add(butCancel);
-		
-		but1.addActionListener(this);
-		but2.addActionListener(this);
-		but3.addActionListener(this);
-		but4.addActionListener(this);
-		but5.addActionListener(this);
-		but6.addActionListener(this);
-		but7.addActionListener(this);
-		but8.addActionListener(this);
-		but9.addActionListener(this);
-		but0.addActionListener(this);
 		
 		butAdd.addActionListener(this);
 		butMinus.addActionListener(this);
 		butMultiply.addActionListener(this);
 		butDivide.addActionListener(this);
-                butSquare.addActionListener(this);
-                butSquareRoot.addActionListener(this);
-                butOneDevidedBy.addActionListener(this);
-                butCos.addActionListener(this);
-                butSin.addActionListener(this);
-                butTan.addActionListener(this);
+		butSquare.addActionListener(this);
+		butSquareRoot.addActionListener(this);
+		butOneDevidedBy.addActionListener(this);
+		butCos.addActionListener(this);
+		butSin.addActionListener(this);
+		butTan.addActionListener(this);
 		
 		butEqual.addActionListener(this);
 		butCancel.addActionListener(this);
-
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		Object source = e.getSource();
-		
-		if(source == but1) {
-			text.append("1");
-		}
-		
-		if(source == but2) {
-			text.append("2");
-		}
 
-		if(source == but3) {
-			text.append("3");
-		}
-		
-		if(source == but4) {
-			text.append("4");
-		}
-		
-		if(source == but5) {
-			text.append("5");
-		}
-		
-		if(source == but6) {
-			text.append("6");
-		}
-		
-		if(source == but7) {
-			text.append("7");
-		}
-		
-		if(source == but8) {
-			text.append("8");
-		}
-		
-		if(source == but9) {
-			text.append("9");
-		}
-		
-		if(source == but0) {
-			text.append("0");
+		for(int i = 0; i < 10; i++) {
+			if(source == but[i]) {
+				text.append(buttonValue[i]);								
+			}
 		}
 		
 		if(source == butAdd) {
@@ -210,40 +152,40 @@ public class UI implements ActionListener {
 			multiply = 0;
 			divide = 1;
 		}
-                
-                if(source == butSquare) {
+
+		if(source == butSquare) {
 			num1 = reader();
-                        result = num1*num1;
+			result = num1*num1;
 			text.setText(Double.toString(result));
 		}
-                
-                if(source == butSquareRoot) {
+
+		if(source == butSquareRoot) {
 			num1 = reader();
-                        result = Math.sqrt(num1);
+			result = Math.sqrt(num1);
 			text.setText(Double.toString(result));
 		}
-                
-                if(source == butOneDevidedBy) {
+
+		if(source == butOneDevidedBy) {
 			num1 = reader();
-                        result = 1/num1;
+			result = 1/num1;
 			text.setText(Double.toString(result));
 		}
-                
-                if(source == butCos) {
+
+		if(source == butCos) {
 			num1 = reader();
-                        result = Math.cos(num1);
+			result = Math.cos(num1);
 			text.setText(Double.toString(result));
 		}
-                
-                if(source == butSin) {
+
+		if(source == butSin) {
 			num1 = reader();
-                        result = Math.sin(num1);
+			result = Math.sin(num1);
 			text.setText(Double.toString(result));
 		}
-                
-                if(source == butTan) {
+
+		if(source == butTan) {
 			num1 = reader();
-                        result = Math.tan(num1);
+			result = Math.tan(num1);
 			text.setText(Double.toString(result));
 		}
 		
@@ -269,7 +211,6 @@ public class UI implements ActionListener {
 				result = num1/num2;
 				text.setText(Double.toString(result));
 			}
-			
 		}
 		
 		if(source == butCancel) {
@@ -277,7 +218,6 @@ public class UI implements ActionListener {
 			num2 = 0.0;
 			text.setText("");
 		}
-	
 	}
 	
 	public double reader() {
@@ -288,5 +228,4 @@ public class UI implements ActionListener {
 		
 		return num;	
 	}
-
 }
