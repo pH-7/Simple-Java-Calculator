a8af9d8348abb7984d84bdf9d01728c2d3bd1a6a
hcz <h.cz@qq.com>
2015-11-17 10:14:36 +0800

    add junit

---
+++ src/simplejavacalculator/SimpleJavaCalculatorTest.java
@@ -0,0 +1,14 @@
+package simplejavacalculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleJavaCalculatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
