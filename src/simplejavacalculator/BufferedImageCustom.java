d10d5a0f94f779a2dafb50e8c4e72b30bb361d91
xdvrx1 <mongAlvarez@gmail.com>
2020-02-17 06:39:17 +0800

    Add files via upload

---
+++ src/simplejavacalculator/BufferedImageCustom.java
@@ -0,0 +1,25 @@
+package simplejavacalculator;
+
+import java.io.*;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.awt.*;
+
+/**
+ *This class will return an image
+ *from a binary data.
+ */
+class BufferedImageCustom {
+   public Image imageReturn() 
+      throws IOException {
+      Image image;
+      
+      //the image here is not even an image file
+      //but rather a binary data
+      InputStream bis = getClass().getResourceAsStream("/icon/icon.png");
+      BufferedImage bImage2 = ImageIO.read(bis);
+      image = bImage2;
+      
+      return image;
+   }       
+}
