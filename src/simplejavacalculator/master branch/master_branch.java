4feacb2c8849dedd70b82e88e6f96b561eaa5057
amdadul-haque <amdadul1807107@gmail.com>
2022-05-08 21:46:40 +0600

    adding first class

---
+++ src/simplejavacalculator/master branch/master_branch.java
@@ -0,0 +1,24 @@
+public class master_branch{
+  int roll;
+  String name;
+
+  master_branch(String n, int r){
+  this.name = n;
+  this.roll = r;
+  }
+
+  public int getRoll(){
+    return roll;
+  }
+  public void setRoll(int roll){
+    this.roll = roll;
+  }
+  public int getName(){
+    return name;
+  }
+  public void setName(String name){
+    this.name = name;
+  }
+
+}
+
