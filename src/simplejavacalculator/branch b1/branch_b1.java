3e39477bb20b44b957e8e33b469098eb2d237f5d
amdadul-haque <amdadul1807107@gmail.com>
2022-05-08 22:05:07 +0600

    commiting from branch b1

---
+++ src/simplejavacalculator/branch b1/branch_b1.java
@@ -0,0 +1,24 @@
+public class branch_b1{
+  int roll;
+  float cgpa
+
+  branch_b1(float n, int r){
+  this.cgpa = n;
+  this.roll = r;
+  }
+
+  public int getRoll(){
+    return roll;
+  }
+  public void setRoll(int roll){
+    this.roll = roll;
+  }
+  public int getCgpa(){
+    return cgpa;
+  }
+  public void setCgpa(float cgpa){
+    this.cgpa = cgpa;
+  }
+
+}
+
