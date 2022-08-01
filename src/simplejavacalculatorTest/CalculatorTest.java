a129cc4f59378670d8df7424ccf36b12ddfe1db8
Lim Yen Yuen <yylimmy@gmail.com>
2022-08-01 13:40:06 +0800

    added inverseCos test
    the test for sin, cos and tan are rerturning errors

--- src/simplejavacalculatorTest/CalculatorTest.java
+++ src/simplejavacalculatorTest/CalculatorTest.java
@@ -98,6 +98,12 @@ class CalculatorTest {
 	}
 	
 	@Test
+	void CalculateMonoInverseCosTest() {
+		Calculator calculator = new Calculator();
+		Assertions.assertEquals(90.0, calculator.calculateMono(Calculator.MonoOperatorModes.inverseCos, 0.0), 0.0000000001);
+	}
+	
+	@Test
 	void CalculateMonoLogTest() {
 		Calculator calculator = new Calculator();
 		Assertions.assertEquals(2.0, calculator.calculateMono(Calculator.MonoOperatorModes.log, 100.0));
