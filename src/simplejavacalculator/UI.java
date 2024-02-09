278d1083e0001e7448f942d21db9ff979b8e2492
Lampros Giotis <Lampyiotis@gmail.com>
2024-02-09 16:44:13 +0200

    Added a new contrast button.It changes the colour of the background and the buttons.When pressed again it turn back to normal

--- src/simplejavacalculator/UI.java
+++ src/simplejavacalculator/UI.java
@@ -18,7 +18,7 @@
 
 package simplejavacalculator;
 
-import java.awt.FlowLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -26,12 +26,10 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
-import java.awt.Font;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 
-import java.awt.Image;
-import javax.swing.ImageIcon; 
+import javax.swing.ImageIcon;
 import java.io.*;
 
 public class UI implements ActionListener {
@@ -49,10 +47,11 @@ public class UI implements ActionListener {
    private final JPanel panelSub8;
    
    private final JTextArea text;
+   private boolean colourCounter;
    
    private final JButton but[], butAdd, butMinus, butMultiply, butDivide,
-      butEqual, butCancel, butSquareRoot, butSquare, butOneDividedBy,
-      butCos, butSin, butTan, butxpowerofy, butlog, butrate, butabs, butBinary, butln;
+      butEqual, butCancel,butSquareRoot, butSquare, butOneDividedBy,
+      butCos, butSin, butTan, butxpowerofy, butlog, butrate, butabs, butBinary, butln,butContrast;
    private final Calculator calc;
    
    private final String[] buttonValue = {"0", "1", "2", "3", "4", "5", "6",
@@ -66,7 +65,7 @@ public class UI implements ActionListener {
    
    public UI() throws IOException {
       frame = new JFrame("Calculator PH");
-      
+
       imageReturn = new BufferedImageCustom();
       image = new ImageIcon(imageReturn.imageReturn());      
       
@@ -86,9 +85,9 @@ public class UI implements ActionListener {
       text = new JTextArea(1, 30);
       
       textFont = new Font("Consolas",Font.BOLD, 24);
-      
-      but = new JButton[10];      
-      for (int i = 0; i < 10; i++) {
+      //colourCounter = false;
+      but = new JButton[11];
+      for (int i = 0; i < 11; i++) {
     		 but[i] = new JButton(String.valueOf(i));
       }      
       butAdd = new JButton("+");      
@@ -106,10 +105,10 @@ public class UI implements ActionListener {
       butxpowerofy = new JButton("x^y");      
       butlog = new JButton("log10(x)");      
       butrate = new JButton("x%");      
-      butabs = new JButton("abs(x)");      
-      butCancel = new JButton("C");      
-      butBinary = new JButton("Bin");      
-      
+      butabs = new JButton("abs(x)");
+      butCancel = new JButton("C");
+      butBinary = new JButton("Bin");
+      butContrast = new JButton("Contrast");
       calc = new Calculator();
       
    }
@@ -124,7 +123,7 @@ public class UI implements ActionListener {
       text.setFont(textFont);
       text.setEditable(false);
       
-      for (int i = 0; i < 10; i++) {
+      for (int i = 0; i < 11; i++) {
          but[i].setFont(font);
       }      
       butAdd.setFont(font);
@@ -144,7 +143,8 @@ public class UI implements ActionListener {
       butrate.setFont(font);
       butabs.setFont(font);
       butCancel.setFont(font);
-      butBinary.setFont(font); 
+      butBinary.setFont(font);
+      butContrast.setFont(font);
       
       panel.add(Box.createHorizontalStrut(100));
       panelSub1.add(text);
@@ -176,7 +176,8 @@ public class UI implements ActionListener {
       
       panelSub5.add(Box.createHorizontalStrut(92));
       panelSub5.add(but[0]);
-      panelSub5.add(butln); 
+      panelSub5.add(butln);
+      panelSub5.add(butContrast);
       panelSub5.add(Box.createHorizontalStrut(210));
       panel.add(panelSub5);
       
@@ -196,8 +197,9 @@ public class UI implements ActionListener {
       panelSub8.add(butabs);
       panelSub8.add(butBinary);
       panel.add(panelSub8);
+      panel.setOpaque(true);
       
-      for (int i = 0; i < 10; i++) {
+      for (int i = 0; i < 11; i++) {
          but[i].addActionListener(this);
       }      
       butAdd.addActionListener(this);
@@ -219,6 +221,7 @@ public class UI implements ActionListener {
       
       butEqual.addActionListener(this);
       butCancel.addActionListener(this);
+      butContrast.addActionListener(this);
       
       frame.add(panel);
       frame.setVisible(true);
@@ -229,7 +232,7 @@ public class UI implements ActionListener {
       final Object source = e.getSource();
       Double checkNum = null;
 
-      for (int i = 0; i < 10; i++) {
+      for (int i = 0; i < 11; i++) {
          if (source == but[i]) {
             text.replaceSelection(buttonValue[i]);
             return;
@@ -307,7 +310,10 @@ public class UI implements ActionListener {
 
          if (source == butBinary)
             parsetoBinary();
+
       }
+      if(source == butContrast)
+         contrastButton();
 
       text.selectAll();
    }
@@ -336,4 +342,164 @@ public class UI implements ActionListener {
          text.setText(Double.toString(num));
       }
    }
+
+   /**
+    * @Author Lampros Giotis
+    * This method implements the contrast function.It switches the colours of the buttons as well as
+    * the panels to make it more contrast.When pressed again it switches back to normal
+    */
+   public void contrastButton(){
+      if(colourCounter == false){//The button has not been pressed.Therfor the starting value remains the same as the initiliazed
+         panel.setBackground(Color.black);//////
+         panelSub1.setBackground(Color.black);//
+         panelSub2.setBackground(Color.black);//
+         panelSub3.setBackground(Color.black);//////Adjusting the black colour to the panels first
+         panelSub4.setBackground(Color.black);//
+         panelSub5.setBackground(Color.black);//
+         panelSub6.setBackground(Color.black);//
+         panelSub7.setBackground(Color.black);//
+         panelSub8.setBackground(Color.black);//
+
+         //We also change the contrast of the buttons.
+         for (int i = 0; i < 11; i++) {
+            but[i].setBackground(Color.white);
+            but[i].setForeground(Color.black);
+         }
+         //==========================================================================
+         butabs.setBackground(Color.white);//We change the colours on the butabs button
+         butabs.setForeground(Color.black);
+         //======================================================================
+         butContrast.setBackground(Color.white);//We change the colours on the contrast button
+         butContrast.setForeground(Color.black);
+         //======================================================================
+         butCancel.setBackground(Color.white);//We change the colours on the cancel button
+         butCancel.setForeground(Color.black);
+         //======================================================================
+         butAdd.setBackground(Color.white);//We change the colours on the add button
+         butAdd.setForeground(Color.black);
+         //======================================================================
+         butTan.setBackground(Color.white);//We change the colours on the tan button
+         butTan.setForeground(Color.black);
+         //======================================================================
+         butDivide.setBackground(Color.white);//We change the colours on the divide button
+         butDivide.setForeground(Color.black);
+         //======================================================================
+         butSin.setBackground(Color.white);//We change the colours on the sin button
+         butSin.setForeground(Color.black);
+         //======================================================================
+         butSquare.setBackground(Color.white);//We change the colours on the square button
+         butSquare.setForeground(Color.black);
+         //======================================================================
+         butSquareRoot.setBackground(Color.white);//We change the colours on the squareroot button
+         butSquareRoot.setForeground(Color.black);
+         //======================================================================
+         butEqual.setBackground(Color.white);//We change the colours on the equal button
+         butEqual.setForeground(Color.black);
+         //=======================================================================
+         butMinus.setBackground(Color.white);//We change the colours on the minus button
+         butMinus.setForeground(Color.black);
+         //=======================================================================
+         butMultiply.setBackground(Color.white);//We change the colours on the multiply button
+         butMultiply.setForeground(Color.black);
+         //=======================================================================
+         butCos.setBackground(Color.white);//We change the colours on the cos button
+         butCos.setForeground(Color.black);
+         //=======================================================================
+         butxpowerofy.setBackground(Color.white);//We change the colours on the powerofy button
+         butxpowerofy.setForeground(Color.black);
+         //=======================================================================
+         butlog.setBackground(Color.white);//We change the colours on the log button
+         butlog.setForeground(Color.black);
+         //=======================================================================
+         butrate.setBackground(Color.white);//We change the colours on the rate button
+         butrate.setForeground(Color.black);
+         //=======================================================================
+         butln.setBackground(Color.white);//We change the colours on the ln button
+         butln.setForeground(Color.black);
+         //=======================================================================
+         butBinary.setBackground(Color.white);//We change the colours on the binary button
+         butBinary.setForeground(Color.black);
+         //Finally...
+         colourCounter = true;//We set the button to true as the action listener has been enabled and the colour is changed to black
+      }else if(colourCounter == true){//The button is pressed
+         panel.setBackground(Color.lightGray);
+         panelSub1.setBackground(Color.lightGray);
+         panelSub2.setBackground(Color.lightGray);
+         panelSub3.setBackground(Color.lightGray);
+         panelSub4.setBackground(Color.lightGray);
+         panelSub5.setBackground(Color.lightGray);
+         panelSub6.setBackground(Color.lightGray);
+         panelSub7.setBackground(Color.lightGray);
+         panelSub8.setBackground(Color.lightGray);
+         //We also change the contrast of the buttons.
+         for (int i = 0; i < 11; i++) {
+            but[i].setBackground(Color.white);
+            but[i].setForeground(Color.black);
+         }
+         //=====================================================================
+         butabs.setBackground(Color.white);//We change the colours on the butabs button
+         butabs.setForeground(Color.black);
+         //======================================================================
+         butContrast.setBackground(Color.white);//We change the colours on the contrasts button
+         butContrast.setForeground(Color.black);
+         //======================================================================
+         butCancel.setBackground(Color.white);//we change the colour on the cancel button
+         butCancel.setForeground(Color.black);
+         //======================================================================
+         butAdd.setBackground(Color.white);//we change the colour on add button
+         butAdd.setForeground(Color.black);
+         //======================================================================
+         butTan.setBackground(Color.white);//we change the colour of the tan button
+         butTan.setForeground(Color.black);
+         //======================================================================
+         butDivide.setBackground(Color.white);//we  change the clour of the division button
+         butDivide.setForeground(Color.black);
+         //======================================================================
+         butSin.setBackground(Color.white);//we change the colour of the sin button
+         butSin.setForeground(Color.black);
+         //======================================================================
+         butSquare.setBackground(Color.white);//we change the colour of the square button
+         butSquare.setForeground(Color.black);
+         //======================================================================
+         butSquareRoot.setBackground(Color.white);//we change the colour the squareroot button
+         butSquareRoot.setForeground(Color.black);
+         //======================================================================
+         butOneDividedBy.setBackground(Color.white);//we change the colour of the deivideby button
+         butOneDividedBy.setForeground(Color.black);
+         //======================================================================
+         butEqual.setBackground(Color.white);//we change the colour of the equal button
+         butEqual.setForeground(Color.black);
+         //=======================================================================
+         butMinus.setBackground(Color.white);//we change the colour of the minus button
+         butMinus.setForeground(Color.black);
+         //=======================================================================
+         butMultiply.setBackground(Color.white);//we change the colour of the multiply button
+         butMultiply.setForeground(Color.black);
+         //=======================================================================
+         butCos.setBackground(Color.white);//we change the colour of the cos button
+         butCos.setForeground(Color.black);
+         //=======================================================================
+         butxpowerofy.setBackground(Color.white);//we change the colour of the powerofy button
+         butxpowerofy.setForeground(Color.black);
+         //=======================================================================
+         butlog.setBackground(Color.white);//we change the colour of the log button
+         butlog.setForeground(Color.black);
+         //=======================================================================
+         butrate.setBackground(Color.white);//we change the colour of the rate button
+         butrate.setForeground(Color.black);
+         //=======================================================================
+         butln.setBackground(Color.white);//we change the colour of the ln button
+         butln.setForeground(Color.black);
+         //=======================================================================
+         butCancel.setBackground(Color.white);//we change the colour of the cancel button
+         butCancel.setForeground(Color.black);
+         //=======================================================================
+         butBinary.setBackground(Color.white);//we change the colour of the binary button
+         butBinary.setForeground(Color.black);
+         colourCounter = false;//We change the value back to false in order to suppose
+         // in some way that the next time the button is pressed,it should change back to black colour.
+
+      }
+   }
+
 }
